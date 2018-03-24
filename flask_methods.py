1a74e1bfa1bf416a3e2880f6189d1fe56e8e49ab
Adri369x <adjsanchez-@hotmail.com>
2018-03-24 18:40:46 +0100

    hola

--- flask_methods.py
+++ flask_methods.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask, session, redirect, url_for, escape, request
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     return 'Welcome!'
@@ -15,8 +14,7 @@ def new_player():
     player = dict()
     player['user_name'] = form['user']
     player['player_ip'] = request.remote_addr
-    with open('players.json', 'a') as players_file:
-        print(json.dumps(player))
+    with open('files/players.json', 'a') as players_file:
         players_file.write(json.dumps(player))
         players_file.write('\n')
     return '200'
@@ -24,7 +22,8 @@ def new_player():
 
 @app.route('/clean_players', methods=['POST'])
 def clean_players():
-    os.remove('players.json')
+    if os.path.isfile('files/players.json'):
+        os.remove('files/players.json')
     return '200'
 
 
